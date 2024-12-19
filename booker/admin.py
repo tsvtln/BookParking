6eaa72a603fe356ea0ac68c6d52900ec6d07550d
TSVTLN <112159858+tsvtln@users.noreply.github.com>
2024-12-19 21:09:35 +0200

    FEAT: added models

--- booker/admin.py
+++ booker/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import ParkingSpace, ParkingAvailability, Booking
 
-# Register your models here.
+
+@admin.register(ParkingSpace)
+class ParkingSpaceAdmin(admin.ModelAdmin):
+    list_display = ('total_spaces', 'created_at', 'updated_at')
+
+
+@admin.register(ParkingAvailability)
+class ParkingAvailabilityAdmin(admin.ModelAdmin):
+    list_display = ('date', 'available_spaces')
+    ordering = ('date',)
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('user', 'date', 'created_at')
+    ordering = ('-created_at',)
--- booker/models.py
+++ booker/models.py
@@ -8,7 +8,7 @@ class Account(models.Model):
     user = models.OneToOneField(
         User,
         on_delete=models.CASCADE,
-        related_name='bookings',
+        related_name='account',
         null=True,
         blank=True,
     )
@@ -62,4 +62,85 @@ class Account(models.Model):
 
     def __str__(self):
         return f"{self.first_name} ({self.nickname}) {self.second_name}"
+
+
+class ParkingSpace(models.Model):
+    total_spaces = models.PositiveIntegerField(
+        help_text='Total number of parking spaces available each day.'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Parking Space {self.total_spaces} total spaces left."
+
+
+class ParkingAvailability(models.Model):
+    date = models.DateField(
+        unique=True,
+        help_text='The date for which availability is being tracked.'
+    )
+
+    available_spaces = models.PositiveIntegerField(
+        help_text="Number of spaces available for parking spaces for this day."
+    )
+
+    class Meta:
+        ordering = ['date']
+
+    def __str__(self):
+        return f'{self.date}: {self.available_spaces}'
+
+
+class Booking(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='bookings',
+        help_text="The user who booked this booking.",
+    )
+
+    date = models.DateField(
+        help_text="The date for the parking is booked.",
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def clean(self):
+        availability = ParkingAvailability.objects.filter(date=self.date).first()
+        if not availability:
+            raise ValidationError(gl('No parking spaces available for this date!'))
+
+        if availability.available_spaces <= 0:
+            raise ValidationError(gl('No spaces available for this date!'))
+
+    def save(self, *args, **kwargs):
+
+        # availability check for the date
+        availability, created = ParkingAvailability.objects.get_or_create(
+            date=self.date,
+            defaults={'available_spaces': ParkingSpace.objects.first().total_spaces},
+        )
+
+        # check bf4 saving
+        if availability.available_spaces <= 0:
+            raise ValidationError(gl('No spaces available for this date!'))
+
+        # deduct space
+        availability.available_spaces -= 1
+        availability.save()
+
+        super().save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        # restore a spot availability
+        availability = ParkingAvailability.objects.filter(date=self.date).first()
+        if availability:
+            availability.available_spaces += 1
+            availability.save()
+
+        super().delete(*args, **kwargs)
+
+    def __str__(self):
+        return f'Booking by {self.user.username} for {self.date}'
 
