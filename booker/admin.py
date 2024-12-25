e3db663728dbf1befd67427c96fa8276cc895fa3
TSVTLN <112159858+tsvtln@users.noreply.github.com>
2024-12-25 10:19:49 +0200

    Update admin.py

--- booker/admin.py
+++ booker/admin.py
@@ -4,16 +4,16 @@ from .models import ParkingSpace, ParkingAvailability, Booking
 
 @admin.register(ParkingSpace)
 class ParkingSpaceAdmin(admin.ModelAdmin):
-    list_display = ('total_spaces', 'created_at', 'updated_at')
+    list_display = ('id', 'total_spaces', 'created_at', 'updated_at')
 
 
 @admin.register(ParkingAvailability)
 class ParkingAvailabilityAdmin(admin.ModelAdmin):
-    list_display = ('date', 'available_spaces')
+    list_display = ('id', 'date', 'available_spaces')
     ordering = ('date',)
 
 
 @admin.register(Booking)
 class BookingAdmin(admin.ModelAdmin):
-    list_display = ('user', 'date', 'created_at')
+    list_display = ('id', 'user', 'date', 'created_at')
     ordering = ('-created_at',)
